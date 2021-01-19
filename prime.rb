335ad99f4be6d43f1b4aa68caa316760e08296f7
Joshua Cho <joshdavecho@protonmail.com>
2021-01-19 09:42:13 -0500

    commit tree c24fa246e6d800843098c98942642d34164620c2

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+    number = 2
+    return false if integer <= 1
+    range = (number..integer-1).to_a
+    range.each do |test_prime|
+        if integer % test_prime == 0
+            return false
+        end
+    end
+    true
+end # Add  code here!
